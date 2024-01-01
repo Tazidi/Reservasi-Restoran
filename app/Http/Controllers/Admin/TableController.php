13888ca3074174a456c63c4bf73da485b36808e8
Tazidi <rizki.tazidi@gmail.com>
2024-01-01 12:08:24 +0700

    crud table

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Table;
 use Illuminate\Http\Request;
+use App\Http\Requests\TableStoreRequest;
 
 class TableController extends Controller
 {
@@ -28,9 +29,16 @@ class TableController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TableStoreRequest $request)
     {
-        //
+        Table::create([
+            'name' => $request->name,
+            'guest_number' => $request->guest_number,
+            'status' => $request->status,
+            'location' => $request->location,
+        ]);
+
+        return to_route('admin.tables.index')->with('success', 'Table created successfully.');
     }
 
     /**
@@ -44,24 +52,32 @@ class TableController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Table $table)
     {
-        //
+        return view('admin.tables.edit', compact('table'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(TableStoreRequest $request, Table $table)
     {
-        //
+        $table->update($request->validated());
+
+        return to_route('admin.tables.index')->with('success', 'Table updated successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function destroy(string $id)
+    public function destroy(Table $table)
     {
-        //
+        $table->reservations()->delete();
+        $table->delete();
+
+        return to_route('admin.tables.index')->with('danger', 'Table daleted successfully.');
     }
 }
--- resources/views/admin/tables/create.blade.php
+++ resources/views/admin/tables/create.blade.php
@@ -1,39 +1,64 @@
 <x-admin-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          {{ __('Dashboard') }}
+      </h2>
+  </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex m-2 p-2">
-                <a href="{{ route('admin.tables.index') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Back to Tables</a>
-            </div>    
-            <div class="container mx-auto p-4 bg-gray-100 rounded-lg shadow-md">
-                <h2 class="text-2xl font-bold mb-4">Add Tables</h2>
-                <form enctype="multipart/form-data">
+  <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="flex m-2 p-2">
+              <a href="{{ route('admin.tables.index') }}"
+                  class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Table Index</a>
+          </div>
+          <div class="container mx-auto p-4 bg-slate-100 rounded-lg shadow-md">
+              <h2 class="text-2xl font-bold mb-4">Create a New Table</h2>
+              <form method="POST" action="{{ route('admin.tables.store') }}">
+                  @csrf
                   <div class="mb-4">
-                    <label for="title" class="block text-sm font-medium text-gray-700">Tables</label>
-                    <input type="text" id="title" wire:model.lazy="title" name="title" class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm sm:leading-5">
+                      <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                      <input type="text" id="name" name="name"
+                          class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm sm:leading-5" />
+                      @error('name')
+                      <div class="text-sm text-red-400">{{ $message }}</div>
+                      @enderror
                   </div>
                   <div class="mb-4">
-                    <label for="image" class="block text-sm font-medium text-gray-700">Image</label>
-                    <input type="file" id="image" wire:model="newImage" name="image" class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm sm:leading-5">
+                      <label for="guest_number" class="block text-sm font-medium text-gray-700">Guest Number</label>
+                      <input type="number" id="guest_number" name="guest_number"
+                          class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm sm:leading-5" />
+                      @error('guest_number')
+                      <div class="text-sm text-red-400">{{ $message }}</div>
+                      @enderror
                   </div>
                   <div class="mb-4">
-                    <label for="body" class="block text-sm font-medium text-gray-700">Description</label>
-                    <textarea id="body" rows="3" wire:model.lazy="body" class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm sm:leading-5"></textarea>
+                      <label for="status" class="block text-sm font-medium text-gray-700">Status</label>
+                      <select id="status" name="status" class="form-multiselect block w-full mt-1">
+                          @foreach (App\Enums\TableStatus::cases() as $status)
+                          <option value="{{ $status->value }}">{{ $status->name }}</option>
+                          @endforeach
+                      </select>
+                      @error('status')
+                      <div class="text-sm text-red-400">{{ $message }}</div>
+                      @enderror
                   </div>
-                  <div>
-                    <button type="submit" class="w-full px-4 py-2 bg-indigo-500 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        Submit
-                    </button>
+                  <div class="mb-4">
+                      <label for="location" class="block text-sm font-medium text-gray-700">Location</label>
+                      <select id="location" name="location" class="form-multiselect block w-full mt-1">
+                          @foreach (App\Enums\TableLocation::cases() as $location)
+                          <option value="{{ $location->value }}">{{ $location->name }}</option>
+                          @endforeach
+                      </select>
+                      @error('location')
+                      <div class="text-sm text-red-400">{{ $message }}</div>
+                      @enderror
                   </div>
-                  
-                </form>
-              </div>
-               
-        </div>
-    </div>
+                  <div class="mt-6 p-4">
+                      <button type="submit"
+                          class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Store</button>
+                  </div>
+              </form>
+          </div>
+      </div>
+  </div>
 </x-admin-layout>
