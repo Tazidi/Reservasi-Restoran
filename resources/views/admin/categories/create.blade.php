0e385b62be3056c53bb026852756e7d8095ea401
Tazidi <rizki.tazidi@gmail.com>
2024-01-01 12:48:48 +0700

    crud reservation & date validation

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -1,40 +1,50 @@
 <x-admin-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          {{ __('Dashboard') }}
+      </h2>
+  </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex m-2 p-2">
-                <a href="{{ route('admin.categories.index') }}" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Back to Category</a>
-            </div>    
-            <div class="container mx-auto p-4 bg-gray-100 rounded-lg shadow-md">
-                <h2 class="text-2xl font-bold mb-4">Create a New Category</h2>
-                <form method="POST" action="{{ route('admin.categories.store') }}" enctype="multipart/form-data">
+  <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="flex m-2 p-2">
+              <a href="{{ route('admin.categories.index') }}"
+                  class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Category Index</a>
+          </div>
+          <div class="container mx-auto p-4 bg-slate-100 rounded-lg shadow-md">
+              <h2 class="text-2xl font-bold mb-4">Create a New Category</h2>
+              <form method="POST" action="{{ route('admin.categories.store') }}" enctype="multipart/form-data">
                   @csrf
                   <div class="mb-4">
-                    <label for="name" class="block text-sm font-medium text-gray-700">Category Name</label>
-                    <input type="text" id="name" name="name" class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm sm:leading-5">
+                      <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                      <input type="text" id="name" name="name"
+                          class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
+                      @error('name')
+                          <div class="text-sm text-red-400">{{ $message }}</div>
+                      @enderror
                   </div>
                   <div class="mb-4">
-                    <label for="image" class="block text-sm font-medium text-gray-700">Image</label>
-                    <input type="file" id="image" name="image" class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm sm:leading-5">
+                      <label for="image" class="block text-sm font-medium text-gray-700">Image</label>
+                      <input type="file" id="image" name="image"
+                          class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('image') border-red-400 @enderror" />
+                      @error('image')
+                          <div class="text-sm text-red-400">{{ $message }}</div>
+                      @enderror
                   </div>
                   <div class="mb-4">
-                    <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
-                    <textarea id="description" name="description" rows="3" class="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm sm:leading-5"></textarea>
+                      <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
+                      <textarea id="description" rows="3" name="description"
+                          class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('description') border-red-400 @enderror"></textarea>
+                      @error('description')
+                          <div class="text-sm text-red-400">{{ $message }}</div>
+                      @enderror
                   </div>
-                  <div>
-                    <button type="submit" class="w-full px-4 py-2 bg-indigo-500 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        Submit
-                    </button>
+                  <div class="mt-6 p-4">
+                      <button type="submit"
+                          class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Store</button>
                   </div>
-                  
-                </form>
-              </div>
-               
-        </div>
-    </div>
+              </form>
+          </div>
+      </div>
+  </div>
 </x-admin-layout>
